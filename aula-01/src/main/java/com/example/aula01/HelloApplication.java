cb9daf993e160da757141e0a36de78d7076a1ebf
tercio.silva <tercio.silva@pmcg.imti>
2023-03-07 22:13:33 -0300

    first commit

---
+++ aula-01/src/main/java/com/example/aula01/HelloApplication.java
@@ -0,0 +1,29 @@
+package com.example.aula01;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        StackPane sp = new StackPane();
+        Label lbl = new Label();
+        lbl.setText("Usando JavaFX");
+        sp.getChildren().add(lbl);
+
+        Scene cena = new Scene(sp, 400, 200);
+        primaryStage.setTitle("Primeiro Projeto");
+        primaryStage.setScene(cena);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
