4940bee32cc2e0be79ae4eed786fae59fa0571ca
tercio.silva <tercio.silva@pmcg.imti>
2023-04-11 22:10:56 -0300

    Rebuilda SICOF

---
+++ SICOF/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module sicof.sicof {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens sicof.sicof to javafx.fxml;
+    exports sicof.sicof;
+}
