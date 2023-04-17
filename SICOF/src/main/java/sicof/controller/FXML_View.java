e9fa6d402a78ec1734849e03c7facff9909f6620
terciotales <terciotalesb@gmail.com>
2023-04-17 13:28:28 -0300

    Fazendo atores

--- SICOF/src/main/java/sicof/controller/FXML_View.java
+++ SICOF/src/main/java/sicof/controller/FXML_View.java
@@ -2,11 +2,11 @@ package sicof.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import sicof.main.Main;
@@ -17,8 +17,8 @@ import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
-public class FXML_View {
-    private String page = "Filmes";
+public class FXML_View implements Initializable {
+    private String page = "Atores";
 
     @FXML
     private Label page_title;
@@ -29,26 +29,35 @@ public class FXML_View {
     @FXML
     private VBox buttons;
 
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            this.loadPage();
+            this.setActiveButton();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     void setPage(MouseEvent event) throws IOException {
         this.page = ((Button) event.getSource()).getText();
+        page_title.setText(page);
+        this.setActiveButton();
+        this.loadPage();
+    }
 
-        ((Button) event.getSource()).getStyleClass().add("active");
-
+    private void setActiveButton() {
         this.buttons.getChildren().forEach(button -> {
-            if (!Objects.equals(((Button) button).getText(), this.page)) {
+            if (Objects.equals(((Button) button).getText(), this.page)) {
+                ((Button) button).getStyleClass().add("active");
+            } else {
                 ((Button) button).getStyleClass().remove("active");
             }
         });
-
-        page_title.setText(page);
-        this.loadPage();
     }
 
     private void loadPage() throws IOException {
         Parent root = null;
-
-        System.out.println("FXML_" + this.page + ".fxml");
 
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("FXML_" + this.page + ".fxml"));
